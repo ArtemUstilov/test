de6ba3c27256289949caae77d25b8d3f20a85ec6
Artem <artem.ustilov@gmail.com>
2018-11-23 18:26:52 +0200

    Updates

---
+++ precache-manifest.356eb82138b808557d572c5d5e58841d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "598d3737990bca92a4ad",
+    "url": "/test/static/js/main.598d3737.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/test/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "598d3737990bca92a4ad",
+    "url": "/test/static/css/main.c77d1435.chunk.css"
+  },
+  {
+    "revision": "863f33314e6d54e62699f686cb36437d",
+    "url": "/test/index.html"
+  }
+];
